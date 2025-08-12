e4de509035291ba4feb78218abae8df019480503
Angelina Zamorska <a.zamorskaya@dmu-medical.com>
2025-08-12 18:05:36 +0300

    build for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -27,11 +27,11 @@ createRoot(document.getElementById('root')).render(
 
 // ---------------------------------------------------
 // Регистрация Service Worker (необязательно)
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then((reg) => console.log('SW registered:', reg.scope))
-      .catch((err) => console.error('SW registration failed:', err));
-  });
-}
+//if ('serviceWorker' in navigator) {
+  //window.addEventListener('load', () => {
+    //navigator.serviceWorker
+      //.register('/sw.js')
+      //.then((reg) => console.log('SW registered:', reg.scope))
+      //.catch((err) => console.error('SW registration failed:', err));
+ // });
+//}
