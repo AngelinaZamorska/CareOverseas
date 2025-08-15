bafff0aef65a6dc803f83f87bcaced6f7a8171d0
Angelina Zamorska <a.zamorskaya@dmu-medical.com>
2025-08-15 20:29:54 +0300

    build for deploy

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,13 +4,14 @@ module.exports = {
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
+  trailingSlash: true,
   sitemapSize: 5000,
   // Языковые версии сайта:
   alternateRefs: [
-    { href: 'https://careoverseas.space/en', hreflang: 'en' },
-    { href: 'https://careoverseas.space/ru', hreflang: 'ru' },
-    { href: 'https://careoverseas.space/pl', hreflang: 'pl' },
-    { href: 'https://careoverseas.space/ar', hreflang: 'ar' },
+    { href: 'https://careoverseas.space/en/', hreflang: 'en' },
+{ href: 'https://careoverseas.space/ru/', hreflang: 'ru' },
+{ href: 'https://careoverseas.space/pl/', hreflang: 'pl' },
+{ href: 'https://careoverseas.space/ar/', hreflang: 'ar' },
     // опционально: "дефолт" для Google (если главная для разных стран одинакова)
     { href: 'https://careoverseas.space/', hreflang: 'x-default' },
   ],
